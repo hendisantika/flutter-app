bdeeabd13c0fc61c999c1d88778f4b4c9982559f
hendisantika <hendisantika@yahoo.co.id>
2024-01-17 13:10:40 +0700

    Add all project codes 🫸🌀✏️📗 :octocat:

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter-appcenter-github-action
+
+A Flutter app to showcase a Github Action to deploy it to AppCenter
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:action_showcase/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: HomePage()));
+}
